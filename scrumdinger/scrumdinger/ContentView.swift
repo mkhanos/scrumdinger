5114499104f437efc1a7198c563c75f9f284f058
Momo Khan <mkhanongm@gmail.com>
2025-04-21 17:21:10 -0400

    Initial project files and package import

---
+++ scrumdinger/scrumdinger/ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView.swift
+//  scrumdinger
+//
+//  Created by Momo Khan on 4/21/25.
+//
+
+import ComposableArchitecture
---
+++ scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Momo Khan on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
