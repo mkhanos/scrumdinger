29f491cca7c40903c7f3b9d23fb97a2c6b439517
Momo Khan <mkhanongm@gmail.com>
2025-04-21 17:49:05 -0400

    Add helper view for sync ups list

--- scrumdinger/scrumdinger/ContentView.swift
+++ scrumdinger/scrumdinger/ContentView.swift
@@ -6,3 +6,21 @@
 //
 
 import ComposableArchitecture
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
